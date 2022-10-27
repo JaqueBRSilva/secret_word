072e6ae90eaf6e6119a9c5766b9b881adacccb9f
JaqueBRSilva <j.silvabroccolo@gmail.com>
2022-10-27 19:32:28 -0300

    feat: finalização tela 'fim de jogo'

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -1,12 +1,15 @@
 import './style.css';
 
-const GameOver = ({ retryGame }) => {
+const GameOver = ({ retry, score }) => {
    return (
-      <div>
-         <h1> GameOver </h1>
-         <button onClick={retryGame}>Resetar jogo</button>
+      <div className="gameover">
+         <h1>Fim de jogo!</h1>
+         <h2>
+            A sua pontuação foi: <span>{score}</span>!
+         </h2>
+         <button onClick={retry}>Reiniciar</button>
       </div>
-   )
-}
+   );
+};
 
-export default GameOver
+export default GameOver;
